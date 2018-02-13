d1d9710c23bc6d88a9b76b6a74a12ba18024f6c6
mrozu <wojtek229@o2.pl>
2018-02-13 14:44:33 +0100

    Add mongoose config and POST /todos

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+app.use(bodyParser.json());
+
+//create new todo
+app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Starting server on port ${port}`);
+});
+
+
