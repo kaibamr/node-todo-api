a96097ba151bd5e238863f42383beea4525f8ece
mrozu <wojtek229@o2.pl>
2018-02-15 17:30:26 +0100

    Add token method

--- server/server.js
+++ server/server.js
@@ -106,9 +106,11 @@ app.post('/users', (req, res) => {
     const body = _.pick(req.body, ['email', 'password']);
     const user = new User(body);
 
-    user.save().then((doc) => {
-        res.send(doc);
-    }, (err) => {
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
