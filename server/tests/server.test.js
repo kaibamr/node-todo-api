200d550941407fb56f43cb8bea5f2abe1afad71a
mrozu <wojtek229@o2.pl>
2018-02-17 20:52:09 +0100

    Add test for logout route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -297,3 +297,22 @@ describe('POST /users/login', () => {
             });       
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should delete a token from user when logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).to.equal(0);
+                    done();
+                }).catch((err)=>{
+                    done(err);
+                });;
+            });
+    });
+})
