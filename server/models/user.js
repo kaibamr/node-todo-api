81ff1bbdc74ac12e259b4710d4f2b9ae0bbce4f9
mrozu <wojtek229@o2.pl>
2018-02-17 20:13:17 +0100

    Add POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,25 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     const User = this;
     let decoded;
--- server/server.js
+++ server/server.js
@@ -121,6 +121,20 @@ app.get('/users/me', authenticate, (req ,res) => {
     res.send(req.user);
 });
 
+//login user
+app.post('/users/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findByCredentials(email, password).then((user) => {
+        return user.generateAuthToken().then((token)=> {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Starting server on port ${port}`);
 });
